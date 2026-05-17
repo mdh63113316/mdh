145f292a31353ed3968f696626fc71798eb4b651
mdh63113316 <1452359101@qq.com>
2026-05-17 10:49:10 +0800

    Refactor JSON responses in acsux.js

--- js/acsux.js
+++ js/acsux.js
@@ -1,15 +1,10 @@
 function home() {
-    return JSON.stringify({
-        class: [{ type_id: '2', type_name: '电影' }],
-        filters: {}
-    });
+    return '{"class":[{"type_id":"1","type_name":"电影"}],"filters":{}}';
 }
 function homeVod() {
-    return JSON.stringify({
-        list: [{ vod_id: 'test', vod_name: '测试数据', vod_pic: '', vod_remarks: '如果看到这条，说明JS已生效' }]
-    });
+    return '{"list":[{"vod_id":"test","vod_name":"如果看到我，说明JS生效了","vod_pic":"","vod_remarks":"请检查分类页"}]}';
 }
-function category(tid, pg) { return homeVod(); }
-function detail(id) { return JSON.stringify({ list: [] }); }
-function play(flag, id) { return JSON.stringify({ parse: 0, url: '' }); }
+function category(tid,pg) { return homeVod(); }
+function detail(id) { return '{"list":[]}'; }
+function play(flag,id) { return '{"parse":0,"url":""}'; }
 function search(wd) { return homeVod(); }
